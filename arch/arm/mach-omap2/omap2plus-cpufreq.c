b9f8f8ee48f73a238cc4fb5aedeac3addbacd30f
kgp700 <neokim20@gmail.com>
2013-02-19 02:28:42 +0900

    FIX: omap2plus-cpufreq

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -391,9 +391,9 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
-	policy->min = policy->cpuinfo.min_freq;
-	policy->max = 384000;
-	policy->cur = 1228800;
+	policy->min = 384000;
+	policy->max = 1228800;
+	policy->cur = omap_getspeed(policy->cpu);
 
 	min_freq = UINT_MAX;
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
